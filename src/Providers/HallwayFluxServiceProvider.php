8c5f00560529b7119b57f7c08110430f2e3849c4
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-04 13:22:25 +0200

    First commit

---
+++ src/Providers/HallwayFluxServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArtisanBuild\Hallway\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HallwayFluxServiceProvider extends ServiceProvider {}
