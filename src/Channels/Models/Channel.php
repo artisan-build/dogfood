956d1661d10e810fcfc1c339a134cc3981c4b442
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-04 23:44:48 +0200

    WIP

--- src/Channels/Models/Channel.php
+++ src/Channels/Models/Channel.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ArtisanBuild\Hallway\Channels\Models;
 
 use ArtisanBuild\Adverbs\Traits\HasVerbsState;
--- src/Members/Models/Member.php
+++ src/Members/Models/Member.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ArtisanBuild\Hallway\Members\Models;
 
 use Illuminate\Database\Eloquent\Model;
