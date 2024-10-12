01550f29cb7d4ff0cd799b00ddbcb0bd6b8a77db
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-12 13:03:52 +0200

    Change Paid to Premium because there might be other ways to be premium member

--- src/Payment/Enums/PaymentStates.php
+++ src/Payment/Enums/PaymentStates.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Payment\Enums;
 
-enum PaymentStates: string
+enum PaymentStates: int
 {
-    case Free = 'Free'; // User has not paid
-    case Paid = 'Paid'; // User has paid and is current
-    case Exempt = 'Exempt'; // Exempt from any payment-related restrictions
-    case Cancelled = 'Cancelled'; // User was paid but has cancelled
-    case GracePeriod = 'GracePeriod'; // User owes and is past due, but on grace period
-    case Suspended = 'Suspended'; // User owes and is suspended until payment received
+    case Free = 0; // User has not paid
+    case Premium = 1; // User has paid and is current
+    case Exempt = 2; // Exempt from any payment-related restrictions
+    case Cancelled = 3; // User was paid but has cancelled
+    case GracePeriod = 4; // User owes and is past due, but on grace period
+    case Suspended = 5; // User owes and is suspended until payment received
 
     public function can(AbilitiesByPaymentStatus $ability): bool
     {
@@ -25,7 +25,7 @@ enum PaymentStates: string
                 AbilitiesByPaymentStatus::PostInFreeChannels,
                 AbilitiesByPaymentStatus::ReadFreeChannels,
             ],
-            self::GracePeriod, self::Paid, self::Exempt => [
+            self::GracePeriod, self::Premium, self::Exempt => [
                 AbilitiesByPaymentStatus::PostInFreeChannels,
                 AbilitiesByPaymentStatus::ReadFreeChannels,
                 AbilitiesByPaymentStatus::PostInPremiumChannels,
