8d061242a73c38532f8f1182c64b2cf51c3c5b71
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-17 00:19:45 +0200

    Move UserState logic over to MemberState

--- src/Members/States/MemberState.php
+++ src/Members/States/MemberState.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Members\States;
 
+use ArtisanBuild\Hallway\Members\Enums\MemberRoles;
 use Thunk\Verbs\State;
 
 class MemberState extends State
 {
     public int|string $user_id;
-    public string $name;
-    public string $handle;
+
+    public string $name = '';
+    public string $handle = '';
+    public string $display_name = '';
+
+    public MemberRoles $role;
+
+
 
     // Member notification preferences
     public array $notify_channel_ids = [];
