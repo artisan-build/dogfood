240fef827e5297b728a9ecd87b070918fe1b6cd1
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-24 00:38:01 +0200

    Started work on pages

---
+++ src/Pages/States/PageState.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArtisanBuild\Hallway\Pages\States;
+
+use Thunk\Verbs\State;
+
+class PageState extends State
+{
+    public string $title;
+    public string $slug;
+
+    public bool $is_lobby = false;
+    public ?string $free_content = null;
+
+    public ?string $premium_content = null;
+
+}
