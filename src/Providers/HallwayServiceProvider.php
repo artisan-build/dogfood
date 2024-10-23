e6af76e34edcfea4f95259f7b5c4b905a4268be4
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-24 00:42:27 +0200

    Create namespaced anonymous component directory

--- src/Providers/HallwayServiceProvider.php
+++ src/Providers/HallwayServiceProvider.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
-class HallwayServiceProvider extends ServiceProvider {}
+class HallwayServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Blade::anonymousComponentNamespace(__DIR__ . '/../../resources/views/components', 'hallway');
+    }
+}
