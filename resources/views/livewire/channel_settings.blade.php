26e86b86a8f2514de418bb2fc703441e79a28b9b
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-24 00:57:54 +0200

    Tweaked channel settings and added a button to create new channels

--- resources/views/livewire/channel_settings.blade.php
+++ resources/views/livewire/channel_settings.blade.php
@@ -1,3 +1,14 @@
+@use('ArtisanBuild\Hallway\Channels\Events\CommunityChannelUpdated;')
 <div>
-    Settings for {{ $channel->name }}
+    <hallway-can :event="CommunityChannelUpdated::class">
+        <flux:card class="space-y-6">
+            <div>
+                <flux:heading size="lg">{{ $channel->name }} Settings</flux:heading>
+                <flux:subheading>You can change your channel's name and type here:</flux:subheading>
+            </div>
+            <livewire:event-form :event="CommunityChannelUpdated::class"
+                                 :state="$channel->verbs_state()"/>
+        </flux:card>
+    </hallway-can>
+
 </div>
--- resources/views/livewire/layout/channels.blade.php
+++ resources/views/livewire/layout/channels.blade.php
@@ -1,5 +1,22 @@
+@php use ArtisanBuild\Hallway\Channels\Events\CommunityChannelCreated; @endphp
 <flux:navlist.group expandable="true" heading="Channels" class="hidden lg:grid">
     @foreach ($channels as $channel)
-        <flux:navlist.item  wire:navigate="true" href="{{ route(config('hallway-flux.route-name-prefix') . 'channel', ['channel' => $channel]) }}" icon="lock-open">{{ $channel->name }}</flux:navlist.item>
+        <flux:navlist.item wire:navigate="true"
+                           href="{{ route(config('hallway-flux.route-name-prefix') . 'channel', ['channel' => $channel]) }}"
+                           icon="lock-open">{{ $channel->name }}</flux:navlist.item>
     @endforeach
+    @if (\Illuminate\Support\Facades\Context::get('active_member')?->can(CommunityChannelCreated::class))
+            <flux:modal.trigger name="add-community-channel">
+                <flux:navlist.item icon="plus">Add Community Channel</flux:navlist.item>
+            </flux:modal.trigger>
+
+            <flux:modal variant="flyout" name="add-community-channel" class="md:w-96 space-y-6">
+                <div>
+                    <flux:heading size="lg">Add Community Channel</flux:heading>
+                    <flux:subheading>Create a new channel for your community.</flux:subheading>
+                </div>
+
+                <livewire:event-form :event="\ArtisanBuild\Hallway\Channels\Events\CommunityChannelCreated::class"/>
+            </flux:modal>
+    @endif
 </flux:navlist.group>
