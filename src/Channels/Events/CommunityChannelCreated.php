d677027d9e9b31c9dc70465a7e5338f539db24f6
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-24 00:41:16 +0200

    Form for channel creation

--- src/Channels/Events/CommunityChannelCreated.php
+++ src/Channels/Events/CommunityChannelCreated.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Channels\Events;
 
+use ArtisanBuild\Adverbs\Traits\ReturnsModelInstanceOnHandle;
+use ArtisanBuild\Adverbs\Traits\SimpleApply;
 use ArtisanBuild\Hallway\Channels\Enums\ChannelTypes;
 use ArtisanBuild\Hallway\Channels\States\ChannelState;
 use ArtisanBuild\Hallway\Members\Enums\MemberRoles;
 use ArtisanBuild\Hallway\Members\Traits\AuthorizesBasedOnMemberState;
+use ArtisanBuild\VerbsFlux\Attributes\EventForm;
+use ArtisanBuild\VerbsFlux\Attributes\EventInput;
+use ArtisanBuild\VerbsFlux\Contracts\RedirectsOnSuccess;
+use ArtisanBuild\VerbsFlux\Enums\InputTypes;
+use Throwable;
 use Thunk\Verbs\Attributes\Autodiscovery\StateId;
 use Thunk\Verbs\Event;
 
+#[EventForm(
+    submit_text: 'Create Community Channel',
+    on_success: RedirectsOnSuccess::class,
+)]
 class CommunityChannelCreated extends Event
 {
     use AuthorizesBasedOnMemberState;
+    use ReturnsModelInstanceOnHandle;
+    use SimpleApply;
 
     public array $authorized_member_roles = [
         MemberRoles::Owner,
@@ -21,21 +34,27 @@ class CommunityChannelCreated extends Event
     ];
 
     #[StateId(ChannelState::class)]
-    public int $channel_id;
+    public ?int $channel_id = null;
 
-    public ChannelTypes $type;
+    #[EventInput(
+        type: InputTypes::Text,
+    )]
     public string $name;
 
+    #[EventInput(
+        type: InputTypes::Select,
+        options: ChannelTypes::class,
+        options_filter: 'isCommunityChannel',
+    )]
+    public ChannelTypes $type;
 
 
-    public function apply(ChannelState $state): void
+
+    /**
+     * @throws Throwable
+     */
+    public function validate(): void
     {
-        $state->name = $this->name;
-        $state->type = $this->type;
-    }
-
-    public function handle(): void
-    {
-        // I play the hand that was dealt, I got a deck full of aces. I gave birth to your style, I need a check for my labor - Lil Wayne
+        $this->assert($this->type->isCommunityChannel());
     }
 }
