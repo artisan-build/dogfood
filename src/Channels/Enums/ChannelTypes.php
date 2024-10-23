ab1f53a965e16cefa5d586ac8486aa45a4db4291
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-24 00:38:50 +0200

    Boolean methods for filtering in the form builder

--- src/Channels/Enums/ChannelTypes.php
+++ src/Channels/Enums/ChannelTypes.php
@@ -12,6 +12,33 @@ enum ChannelTypes: int
     case PrivateFree = 2;
     case PrivatePremium = 3;
 
+    case ReadOnlyFree = 4;
+    case ReadOnlyPremium = 5;
+    case Blog = 6;
+
     // Member Channels
     case Member = 11;
+    case MemberPrivate = 12;
+
+    public function isCommunityChannel(): bool
+    {
+        return in_array($this, [
+            self::OpenFree,
+            self::OpenPremium,
+            self::PrivateFree,
+            self::PrivatePremium,
+            self::ReadOnlyFree,
+            self::ReadOnlyPremium,
+            self::Blog,
+        ], true);
+    }
+
+    public function isMemberChannel(): bool
+    {
+        return in_array($this, [
+            self::Member,
+            self::MemberPrivate,
+        ], true);
+
+    }
 }
