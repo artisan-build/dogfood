f9893fcf1da074a70d362d15cd1bfaa9c3a226a6
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-10-26 17:10:27 +0200

    Replace edit-profile with edit-page

--- resources/views/livewire/lobby.blade.php
+++ resources/views/livewire/lobby.blade.php
@@ -2,11 +2,11 @@
 <div>
     <x-hallway::can :event="PageUpdated::class">
         <div class="float-right">
-            <flux:modal.trigger name="edit-profile">
+            <flux:modal.trigger name="edit-page">
                 <flux:button>Edit This Page</flux:button>
             </flux:modal.trigger>
 
-            <flux:modal name="edit-profile" variant="flyout" class="space-y-6">
+            <flux:modal name="edit-page" variant="flyout" class="space-y-6">
                 <livewire:event-form :event="\ArtisanBuild\Hallway\Pages\Events\PageUpdated::class"
                                      :state="$lobby->verbs_state()"/>
             </flux:modal>
