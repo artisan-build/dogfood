83e7846d8d56f9822b9115948764e2d82376d49b
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-11-07 16:19:23 +0100

    Members can now enter and leave channels

--- resources/views/livewire/channels.blade.php
+++ resources/views/livewire/channels.blade.php
@@ -1,14 +1,29 @@
-<div class="flex">
-    @foreach ($channels as $channel)
-        <flux:card>
-            <flux:heading size="lg">Are you sure?</flux:heading>
+<flux:table class="p-4">
+    <flux:columns>
+        <flux:column>Name</flux:column>
+        <flux:column>Members</flux:column>
+        <flux:column>In Channel</flux:column>
+        <flux:column>Notifications</flux:column>
+    </flux:columns>
 
-            <flux:subheading class="mb-4">
-                <p>Your post will be deleted permanently.</p>
-                <p>This action cannot be undone.</p>
-            </flux:subheading>
-
-            <flux:button variant="danger">Delete</flux:button>
-        </flux:card>
-    @endforeach
-</div>
+    <flux:rows>
+        @foreach ($channels as $channel)
+            @if ($channel->availableToMember())
+            <flux:row>
+                <flux:cell>
+                    <flux:tooltip :content="$channel->description">
+                        <flux:button class="underline decoration-dashed" variant="ghost">{{ $channel->name }}</flux:button>
+                    </flux:tooltip>
+                </flux:cell>
+                <flux:cell>{{ count($channel->member_ids) }}</flux:cell>
+                <flux:cell>
+                    <flux:switch wire:model.live="membership.{{$channel->id}}"/>
+                </flux:cell>
+                <flux:cell class="space-x-6">
+                    Coming Soon
+                </flux:cell>
+            </flux:row>
+            @endif
+        @endforeach
+    </flux:rows>
+</flux:table>
