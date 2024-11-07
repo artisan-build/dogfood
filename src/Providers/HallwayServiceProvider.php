0aad8fadb17c90baa658da92fbe8ae7872183590
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-11-07 16:23:18 +0100

    Some array macros to make things a bit easier

--- src/Providers/HallwayServiceProvider.php
+++ src/Providers/HallwayServiceProvider.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Providers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class HallwayServiceProvider extends ServiceProvider
 {
+    public function register(): void {}
     public function boot(): void
     {
+        Arr::macro('addUniqueToList', function (array $array, mixed $value): array {
+            $array[] = $value;
+            return array_unique($array);
+        });
+
+        Arr::macro('removeFromList', fn(array $array, mixed $value): array => collect($array)->filter(fn($val) => $val !== $value)->toArray());
         Blade::anonymousComponentPath(__DIR__ . '/../../resources/views/components', 'hallway');
     }
 }
