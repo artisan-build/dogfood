414834897c4e8ddbbcc12c0597389e0a04d3a401
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-11-15 15:23:22 +0100

    Get channel context from threads

--- src/Channels/Middleware/SetActiveChannelInContext.php
+++ src/Channels/Middleware/SetActiveChannelInContext.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ArtisanBuild\Hallway\Channels\Middleware;
 
+use ArtisanBuild\Hallway\Channels\States\ChannelState;
+use ArtisanBuild\Hallway\Messages\States\MessageState;
 use Closure;
 use Context;
 use Illuminate\Http\Request;
@@ -17,7 +19,13 @@ class SetActiveChannelInContext
         }
         if ($channel = $request->route('channel')) {
             Context::add('channel', $channel);
-        } else {
+        }
+        elseif ($thread = $request->route('message')) {
+            Context::add('thread', $thread);
+            assert($thread instanceof MessageState);
+            Context::add('channel', ChannelState::load($thread->channel_id));
+        }
+        else {
             Context::forget('channel');
         }
 
