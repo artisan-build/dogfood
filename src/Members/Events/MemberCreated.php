cd1b8c67a5e1069864c535633fa91abaffc36f3d
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2024-11-18 14:31:39 +0100

    Fix seeder so that it populates the payment and moderation states

--- src/Members/Events/MemberCreated.php
+++ src/Members/Events/MemberCreated.php
@@ -7,6 +7,8 @@ namespace ArtisanBuild\Hallway\Members\Events;
 use ArtisanBuild\Adverbs\Traits\SimpleApply;
 use ArtisanBuild\Hallway\Members\Enums\MemberRoles;
 use ArtisanBuild\Hallway\Members\States\MemberState;
+use ArtisanBuild\Hallway\Moderation\Enums\ModerationMemberStates;
+use ArtisanBuild\Hallway\Payment\Enums\PaymentStates;
 use Thunk\Verbs\Attributes\Autodiscovery\StateId;
 use Thunk\Verbs\Event;
 
@@ -21,6 +23,8 @@ class MemberCreated extends Event
     public int $user_id;
 
     public MemberRoles $role = MemberRoles::Member;
+    public PaymentStates $payment_state = PaymentStates::Free;
+    public ModerationMemberStates $moderation_state = ModerationMemberStates::Active;
 
     public string $handle;
 
