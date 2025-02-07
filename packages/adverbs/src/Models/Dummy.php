f477108950c338b68f16d0f7c614a5b2756a2d87
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-07 23:24:21 +0100

    docs: add comprehensive PHPDoc blocks to models

--- packages/adverbs/src/Models/Dummy.php
+++ packages/adverbs/src/Models/Dummy.php
@@ -9,10 +9,33 @@ use ArtisanBuild\Adverbs\Traits\GetsRowsFromVerbsStates;
 use ArtisanBuild\Adverbs\Traits\HasVerbsState;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string|null $name
+ * @property string|null $description
+ * @property string|null $metadata
+ * @property int|null $id
+ * @property string|null $last_event_id
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereLastEventId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereMetadata($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereName($value)
+ *
+ * @mixin \Eloquent
+ */
 class Dummy extends Model
 {
     use GetsRowsFromVerbsStates;
     use HasVerbsState;
 
     public string $stateClass = DummyState::class;
+
+    protected function getStateClass(): string
+    {
+        return \Thunk\Verbs\State::class;
+    }
 }
