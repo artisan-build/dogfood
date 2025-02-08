808ca39473cb646a891cd9625ab8db042d7447a5
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-08 10:32:46 +0100

    refactor: clean up code style and implement proper event handling for API tokens and authentication

--- packages/adverbs/src/Models/Dummy.php
+++ packages/adverbs/src/Models/Dummy.php
@@ -10,13 +10,12 @@ use ArtisanBuild\Adverbs\Traits\HasVerbsState;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
- *
  * @property string|null $name
  * @property string|null $description
  * @property string|null $metadata
  * @property int|null $id
  * @property string|null $last_event_id
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy query()
@@ -25,6 +24,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereLastEventId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereMetadata($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Dummy whereName($value)
+ *
  * @mixin \Eloquent
  */
 class Dummy extends Model
