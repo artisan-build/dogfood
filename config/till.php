ee45e65569a9a5a937d624140dec607f93a4b68d
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-24 16:00:57 +0100

    Stream prep

---
+++ config/till.php
@@ -0,0 +1,29 @@
+<?php
+
+use ArtisanBuild\Till\Enums\PaymentProcessors;
+use ArtisanBuild\Till\Models\TillTeam;
+
+return [
+    'payment_processor' => PaymentProcessors::Demo,
+    'team_mode' => true,
+    'team_model' => \App\Models\Team::class,
+    'user_model' => \App\Models\User::class,
+    'live_or_test' => env('TILL_LIVE_OR_TEST'), // null means that we use the live price in production and test price everywhere else
+    'subscribe_uri' => env('TILL_SUBSCRIBE_URI', 'subscribe'),
+    'pricing_uri' => env('TILL_PRICING_URI', 'pricing'),
+    'plans_uri' => env('TILL_PLANS_URI', 'plans'),
+    'default_display' => env('TILL_DEFAULT_DISPLAY', 'year'),
+    'always_show_lifetime' => true,
+    'plan_path' => app_path('Plans'),
+    'pricing_section_template' => 'till::livewire.pricing_section',
+    'active_feature_icon' => null,
+    'inactive_feature_icon' => null,
+    'update_ledgers' => [
+        'hourly' => true,
+        'daily' => true,
+        'weekly' => true,
+        'monthly' => true,
+        'yearly' => true,
+    ],
+
+];
