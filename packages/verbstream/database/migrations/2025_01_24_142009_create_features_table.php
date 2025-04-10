bafcca64d1be5580819ac46513f321e9da1bddac
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-04-10 13:55:27 +0200

    Linting

--- packages/verbstream/database/migrations/2025_01_24_142009_create_features_table.php
+++ packages/verbstream/database/migrations/2025_01_24_142009_create_features_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('features', function (Blueprint $table) {
+        Schema::create('features', function (Blueprint $table): void {
             $table->id();
             $table->string('name');
             $table->string('scope');
